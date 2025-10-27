cca5e9dc6dd109a2608ce2fddc1740cab89506aa
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-10-27 17:22:52 +0100

    Improve gadget URL handling and error responses
    Adds named URL for gadget slugs to enhance reverse lookups and
    streamlines gadget detail view with redirects for better UX.
    
    Introduces more robust error handling, including 404 responses for
    missing gadgets by slug or ID.

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, Http404
 from .dummy_data import gadgets, manufacturers
 import json
 from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your views here.
 
@@ -12,13 +13,19 @@ def start_page_view(request):
 
 
 def single_gadget_view(request, gadget_id):
-    return JsonResponse(slugify(gadgets[gadget_id]['name']), safe=False)
+    if len(gadgets) > gadget_id:
+        new_url = reverse('gadget_slug_url', args=[
+            slugify(gadgets[gadget_id]['name'])])
+        return redirect(new_url)
+    return HttpResponseNotFound("Gadget not found by ID")
 
 
 def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = {"result": "nothing"}
+    gadget_match = None
     for gadget in gadgets:
         if slugify(gadget['name']) == gadget_slug:
             gadget_match = gadget
             break
+    if not gadget_match:
+        raise Http404("Gadget not found by slug")
     return JsonResponse(gadget_match, safe=False)
